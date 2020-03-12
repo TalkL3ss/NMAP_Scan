fbb943dd8a52d70927b1150efc1005a6c34a6ec1
TalkL3ss <30426929+TalkL3ss@users.noreply.github.com>
2020-03-12 13:13:43 +0200

    Update Nmapper.ps1

--- Nmapper.ps1
+++ Nmapper.ps1
@@ -1,26 +1,29 @@
-#TODO: split the config from the function + do not send report if no changes are made
-$cFile = 'c:\temp\Current.csv'
+#General Settings
+$global:cFile = 'c:\temp\Current.csv'
 $oFile = 'c:\temp\OldVer.csv'
-cp $cFile $oFile -Force
 $max = 20
+cp $cFile $oFile -Force #copy the last scan report to old one for latest compare
+'"IP","PORT"' | tee $cFile
+$CompFile =  'C:\temp\RunNewOld.csv'
+
+#Start Nmap With No Port and only ping to get all alive hosts, and strip the output to ip only
 $nmapper = (nmap -sP 0.0.0.0/24 | Select-String -Pattern "\d{1,3}(\.\d{1,3}){3}" -AllMatches).Matches.Value
-rm $cFile -Force -Confirm:$false
-'"IP","PORT"' | tee $cFile -Force 
+
 
 Function AsyncJobs($ip) {
+#Scan for all open ports in the list
 $sb = { param($ip)
-        $results = (nmap -R -sS -n -T4 --open -p- $ip)
+        $results = (nmap -R -sS -T4 --open -p- $ip)
         $oPorts = (($results | Select-String -Pattern "\d{1,5}/tcp" -AllMatches).Matches.Value | select @{ Name = 'Port';  Expression = {$_}},@{ Name = 'IP';  Expression = {($results | Select -First 2 | select -Last 1).split(" ")[4]}})
-        $oPorts | Export-Csv -Path $cFile -Append -NoClobber -NoTypeInformation
+        $oPorts | Export-Csv -Path 'c:\temp\Current.csv' -Append -NoClobber -NoTypeInformation
         }
 
          if (!((Get-Job -State Running).Count -lt $max)) { (Get-Job -State Running) | Wait-Job -Any }
             Start-Job -Name $ip -ScriptBlock $sb -ArgumentList $ip | out-null
-
-
 } 
 
-$nmapper | select -First 4 |  % -parallel { AsyncJobs -ip $_  }
+$nmapper | % { AsyncJobs -ip $_  }
 Get-Job * | Wait-Job 
-Compare-Object (gc 'C:\temp\Current.csv') (gc 'C:\temp\OldVer.csv') | select @{N='Address'; exp= {($_.InputObject).split(",")[1]}},@{N='Port'; exp= {($_.InputObject).split(",")[0]}},@{N='Removed/Add'; exp= {if ($_.SideIndicator -like "<=") { "Added" } elseif ($_.SideIndicator -like "=>") { "Removed" }}} | tee C:\temp\RunNewOld.csv
-ii C:\temp\RunNewOld.csv
+$CompObjs =  Compare-Object (gc $cFile) (gc $oFile) | select @{N='Address'; exp= {($_.InputObject).split(",")[1]}},@{N='Port'; exp= {($_.InputObject).split(",")[0]}},@{N='Removed/Add'; exp= {if ($_.SideIndicator -like "<=") { "Added" } elseif ($_.SideIndicator -like "=>") { "Removed" }}} 
+if ($CompObjs.Count -ge 1) { $CompObjs | tee $CompFile }
+else { "No Chnages Are Made" | tee $CompFile }
